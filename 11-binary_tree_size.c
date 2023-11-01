013f1b17c6d6d0d5261e06d15ed64f2cf3544d5f
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-01 14:12:00 +0200

    11-size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,16 +6,16 @@
 * Return: the size of the tree
 */
 size_t binary_tree_size(const binary_tree_t *tree)
-{   
-    size_t size = 1;
+{
+	size_t size = 1;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    size += binary_tree_size(tree->right);
-    size += binary_tree_size(tree->left);
-    return (size);
+	size += binary_tree_size(tree->right);
+	size += binary_tree_size(tree->left);
+	return (size);
 
 }
