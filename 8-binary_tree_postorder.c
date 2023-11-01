a750bacbfa80758c02dd02a96b59e0877a994560
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-01 14:14:41 +0200

    betty

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,8 +7,9 @@
 * Return: Nothing
 */
 
-gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c 9-binary_tree_height.c 9-main.c 0-binary_tree_node.c 2-binary_tree_insert_right.c -o 9-height{
-	if (tree == NULL || func == NULL)
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NUll || func == NULL)
 	{
 		return;
 	}
