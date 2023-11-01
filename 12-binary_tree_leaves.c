b7bd75fb51ce2eb54e6e72bd9859b7e013753b36
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-11-01 14:51:24 +0200

    13-binary

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,16 +7,17 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves = 0;
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (!tree->right && !tree->left)
-    {
-        leaves += 1;
-    }
-    leaves += binary_tree_leaves(tree->right);
-    leaves += binary_tree_leaves(tree->left);
-    return (leaves);
+	size_t leaves = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (!tree->right && !tree->left)
+	{
+		leaves += 1;
+	}
+	leaves += binary_tree_leaves(tree->right);
+	leaves += binary_tree_leaves(tree->left);
+	return (leaves);
 }
